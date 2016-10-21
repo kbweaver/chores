9d1400deb8c5d0e12414d14679231ac6dc76ef3a
Joshua Ochakovsky <josh@ochakovsky.com>
2016-10-20 17:06:29 -0700

    Change to chores.ochakovsky.com url
    TODO: figure out best way to not hard-code url

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :chores, Chores.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "shrouded-shore-11667.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "chores.ochakovsky.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
