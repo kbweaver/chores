3e599de2d7ab29ff4f6302256cda6e7fde867718
Joshua Ochakovsky <josh@ochakovsky.com>
2016-10-22 00:59:50 -0700

    Don't reload entire chores list when adding a chore

--- web/static/app/actions/index.js
+++ web/static/app/actions/index.js
@@ -8,7 +8,6 @@ function receiveChores(json) {
   return {
     type: 'RECEIVE_CHORES',
     chores: json.data,
-    receivedAt: Date.now(),
   };
 }
 
@@ -21,14 +20,23 @@ export function fetchChores() {
   };
 }
 
+function receiveNewChore(json) {
+  return {
+    type: 'RECEIVE_NEW_CHORE',
+    newChore: json.data,
+  };
+}
+
 export function addChore(name, date) {
   return dispatch => {
+    // TODO: dispatch an action indicating that a new chore has been sent to the
+    // server.
     return fetch('/api/chores', {
       method: 'POST',
       body: JSON.stringify({ chore: { name: name, date: date.toISOString() } }),
       headers: new Headers({ "Content-Type": "application/json" }),
     })
-      // TODO: optimistic updates?
-      .then(() => dispatch(fetchChores()));
+      .then(response => response.json())
+      .then(json => dispatch(receiveNewChore(json)));
   }
 }
--- web/static/app/reducers/chores.js
+++ web/static/app/reducers/chores.js
@@ -1,11 +1,23 @@
-const chores = (state = { items: [] }, action) => {
+const initialState = {
+  items: [],
+  isFetching: false,
+}
+
+const chores = (state = initialState, action) => {
   switch (action.type) {
     case 'RECEIVE_CHORES':
       return Object.assign({}, state, {
         items: action.chores,
-        receivedAt: action.receivedAt,
+        isFetching: false,
       });
-    case 'REQUEST_CHORES': // TODO: keep track of in-progress request
+    case 'REQUEST_CHORES':
+      return Object.assign({}, state, {
+        isFetching: true,
+      });
+    case 'RECEIVE_NEW_CHORE':
+      return Object.assign({}, state, {
+        items: state.items.concat([action.newChore]),
+      });
     default:
       return state;
   }
