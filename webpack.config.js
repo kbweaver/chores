ca27dab63fca27c3967a416b36f978c31f3bda5e
Joshua Ochakovsky <josh@ochakovsky.com>
2016-10-25 00:52:48 -0700

    CSS source maps

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     {
       test: /\.css$/,
-      loader: ExtractTextPlugin.extract("style-loader", "css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]") 
+      loader: ExtractTextPlugin.extract("style-loader", "css-loader?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]") 
     }],
   },
   output: {
